3271a351a398002c267682ee2bf3174a9ad3d6bb
unknown <mdirfan29pro@gmail.com>
2023-09-23 13:02:36 +0530

    error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navigationbar from "./Navbar";
 import Input from "./Input";
 import Random from "./Random";
 
-function App(){
-    return (<div>
-        <Navigationbar/> 
-        <BrowserRouter>
+function App() {
+  return (
+    <div>
+      <Navigationbar />
+      <BrowserRouter basename="/">
         <Routes>
-            <Route path="/" element={<Input/>} />
-            <Route path="/random" element={<Random/>} />
-            </Routes>
-        </BrowserRouter>
-    </div>);
+          <Route exact path="/" element={<Input />} />
+          <Route path="/random" element={<Random />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
