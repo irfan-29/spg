bb84c8b0f69e6f6bbc6fc46c0249c848d455e0c6
unknown <mdirfan29pro@gmail.com>
2023-09-24 12:07:54 +0530

    latest-ui-1

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,8 @@ import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import Alert from 'react-bootstrap/Alert';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
@@ -27,11 +29,27 @@ function Input() {
     setDob(newValue);
   }
   function handleSpecialChange() {
+    const special = "~!@#$%^&";
+    const newChar = special.charAt(Math.floor(Math.random() * special.length));
     setName((prevValue) => {
-      if (prevValue.includes("@")) {
-        return prevValue.replace("@", "");
+      if (prevValue.includes('@')) {
+        return prevValue.replace('@', '');
+      }else if (prevValue.includes('~')) {
+        return prevValue.replace('~', '');
+      }else if (prevValue.includes('#')) {
+        return prevValue.replace('#', '');
+      }else if (prevValue.includes('!')) {
+        return prevValue.replace('!', '');
+      }else if (prevValue.includes('$')) {
+        return prevValue.replace('$', '');
+      }else if (prevValue.includes('%')) {
+        return prevValue.replace('%', '');
+      }else if (prevValue.includes('^')) {
+        return prevValue.replace('^', '');
+      }else if (prevValue.includes('&')) {
+        return prevValue.replace('&', '');
       } else {
-        return prevValue + "@";
+        return prevValue + newChar;
       }
     });
   }
@@ -49,42 +67,28 @@ function Input() {
   result = result.substring(0, number);
 
   return (  
-    <div style={{ margin: "60px 5px" }}>
+    <div className="display">
       <ButtonGroup size="md" className="mb-2 navigation">
         <Button variant="light" href="/" className="px-5 btn">Create Password</Button>
         <Button variant="dark" href="/random" className="px-5 btn">Generate Password</Button>
       </ButtonGroup>
       <br/>
       <br/>
-      <label for="name">Enter the Name: </label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        onChange={handleNameChange}
-        value={name}
-        style={{ margin: "15px" }}
-      />
-      <br />
-      <label for="dob">Enter the DOB: </label>
-      <input
-        type="date"
-        name="dob"
-        id="dob"
-        onChange={handleDobChange}
-        style={{ margin: "15px" }}
-      />
-      <br />
-      <label for="number">Enter the size: </label>
-      <input
-        type="number"
-        name="number"
-        id="number"
-        onChange={handleNumberChange}
-        value={number}
-        style={{ margin: "15px" }}
-      />
-      <br />
+      <br/>
+       
+      {alert ? <Alert variant="success" className="alert" onClose={() => setAlert(false)} dismissible>🥳 Your Password has been saved successfully to your clipboard</Alert> : ""}
+     
+      <FloatingLabel controlId="floatingInput" width="40%" label="Name" className="mb-4 ip">
+        <Form.Control type="name" id="name" name="name" onChange={handleNameChange} value={name} placeholder="Name"/>
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingDOB" label="DOB" className="mb-4 ip">
+        <Form.Control type="date" name="dob" id="dob" onChange={handleDobChange} placeholder="dob"/>
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingNumber" label="Size" className="mb-4 ip">
+        <Form.Control type="number" name="number" id="number" onChange={handleNumberChange} value={number} placeholder="Number"/>
+      </FloatingLabel>
+      
+      <div className="checkboxes">
       <input
         type="checkbox"
         name="special"
@@ -102,16 +106,17 @@ function Input() {
         style={{ margin: "15px" }}
       />
       <label for="caps">Include Capital letters</label>
-      <br/>
-      <div className="copy">    
-        {result!=="" && <h1 className="copyContent">{result}</h1> }   
-        <CopyToClipboard text={result}>
-          <ContentCopyIcon fontSize="large" onClick={() => setAlert(true)} className="copyIcon"/>
-        </CopyToClipboard>
-        </div>
-      <br/>   
-      {alert ? <Alert variant="success" className="alert" onClose={() => setAlert(false)} dismissible>Success</Alert> : ""}
+      </div>
       
+      <br/>
+
+      <div className="copy">    
+        {result!=="" && <h1 className="copyContent">{result}
+        <CopyToClipboard text={result}>
+          <ContentCopyIcon fontSize="medium" onClick={() => setAlert(true)} className="copyIcon"/>
+        </CopyToClipboard></h1> }   
+        
+        </div>
     </div>
   );
 }
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,58 +1,94 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import Alert from 'react-bootstrap/Alert';
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import Alert from "react-bootstrap/Alert";
 // import { Link } from "react-router-dom";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-function Random(){
+function Random() {
+  const [number, setNumber] = React.useState(12);
+  const [alert, setAlert] = React.useState(false);
 
-    const [number, setNumber] = React.useState(12);
-    const [alert, setAlert] = React.useState(false);
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    function generateString(length) {
-        let result = ' ';
-        const charactersLength = characters.length;
-        for ( let i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
+  function generateString(length) {
+    let result = "";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    function handleNumberChange(event){
-        const value = event.target.value;
-        setNumber(value);
-    }
+    return result;
+  }
+  function handleNumberChange(event) {
+    const value = event.target.value;
+    setNumber(value);
+  }
 
-    const result = generateString(number);
+  var result = generateString(number);
 
-    return (
-    <div style={{ margin: "50px 5px" }}>
-    <ButtonGroup size="md" className="mb-2 navigation">
-        <Button variant="dark" href="/" className="px-5 btn">Create Password</Button>
-        <Button variant="light" href="/random" className="px-5 btn">Generate Password</Button>
+  return (
+    <div style={{ margin: "50px 5px", textAlign: "center" }}>
+      <ButtonGroup size="md" className="mb-2 navigation">
+        <Button variant="dark" href="/" className="px-5 btn">
+          Create Password
+        </Button>
+        <Button variant="light" href="/random" className="px-5 btn">
+          Generate Password
+        </Button>
       </ButtonGroup>
-      <br/>
-      <br/>
-        <label for="number">Enter the size: </label>
-        <input type="number" name="number" id="number" onChange={handleNumberChange} value={number} style={{margin: "15px"}}/>
-        <br/> 
+      <br />
+      <br />
+      <br />
 
-        <div className="copy">
-        {result!=="" && <h1 className="copyContent">{result}</h1> } 
+      {alert ? (
+        <Alert
+          variant="success"
+          className="alert"
+          onClose={() => setAlert(false)}
+          dismissible
+        >
+          🥳 Your Password has been saved successfully to your clipboard
+        </Alert>
+      ) : (
+        ""
+      )}
+
+      <FloatingLabel
+        controlId="floatingNumber"
+        label="Enter the size"
+        className="mb-4 ip"
+      >
+        <Form.Control
+          type="number"
+          name="number"
+          id="number"
+          onChange={handleNumberChange}
+          value={number}
+          placeholder="Number"
+        />
+      </FloatingLabel>
+
+      <br />
+
+      <div className="copy">
+        {result!=="" && <h1 className="copyContent">{result}
         <CopyToClipboard text={result}>
-          <ContentCopyIcon fontSize="large" onClick={() => setAlert(true)} className="copyIcon"/>
-        </CopyToClipboard>
-
-        </div>
-      <br/>
-      
-      {alert ? <Alert variant="success" className="alert" onClose={() => setAlert(false)} dismissible>Copied!</Alert> : ""}
-      
-    </div>);
+          <ContentCopyIcon
+          title="hello"
+            fontSize="medium"
+            onClick={() => setAlert(true)}
+            className="copyIcon"
+          />
+        </CopyToClipboard></h1> } 
+        
+      </div>
+    </div>
+  );
 }
 
 export default Random;
